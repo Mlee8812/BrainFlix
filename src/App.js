7e0661ee556eb62902720266ad8fe7308568b1e2
mlee8812 <mlee8812@gmail.com>
2024-01-06 15:37:43 -0500

    sprint-2 updates

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
 import React from "react";
-import "./App.scss";
-
-//Compenents
-import Header from "./Components/Header/header";
-import CommentForm from "./Components/CommentForm/CommentForm";
-import MainVideo from "./Components/MainVideo/MainVideo";
-import VideoBuilder from "./Components/VideoBuilder/VideoBuilder";
-import CommentBuilder from "./Components/CommentBuilder/CommentBuilder";
-import VideoInfo from "./Components/VideoInfo/VideoInfo";
-
-//Data
-import VideoDetails from "./Data/video-details.json";
-import VideoList from "./Data/videos.json";
-
-//This is my only class component
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Header from "./components/Header/header";
+import Upload from "./components/Upload/Upload";
 
 class App extends React.Component {
-  state = {
-    VideoDetails: VideoDetails,
-
-    CurrentVideo: VideoDetails[0],
-  };
-
-  updateVideo = (VideoDetails) => {
-    console.log(VideoDetails);
-    this.setState({
-      CurrentVideo: VideoDetails,
-    });
-  };
-
   render() {
     return (
-        <div className="App">
+        <BrowserRouter>
           <Header />
-          <MainVideo CurrentVideo={this.state.CurrentVideo} />
-          <div className="container-column">
-            <div className="main-column">
-              <VideoInfo CurrentVideo={this.state.CurrentVideo} />
-              <CommentForm />
-              <CommentBuilder CurrentVideo={this.state.CurrentVideo} />
-            </div>
-            <aside className="aside-column">
-              <VideoBuilder VideoList={this.state.VideoDetails} updateVideo={this.updateVideo} />{" "}
-            </aside>
-          </div>
-        </div>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route
+                path="/video/:id"
+                render={(props) => {
+                  return <Home {...props} />;
+                }}
+            />
+            <Route path="/upload" component={Upload} />
+          </Switch>
+        </BrowserRouter>
     );
   }
 }
